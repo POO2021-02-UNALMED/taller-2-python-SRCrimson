13719cadd863b09090af63aa2904d3fc3d8a782e
SRCrimson <92110773+SRCrimson@users.noreply.github.com>
2021-10-26 21:23:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 class Asiento:
+    pass
     def __init__(self, precio, color, registro):
         self.precio   = precio 
         self.color    = color
         self.registro = registro
      
-    def cambiarColor(self,color):
-        if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
-            self.color=color
+    def cambiarColor(self, color):
+        if color=='rojo' or color=='verde' or color=='amarillo' or color=='negro' or color=='blanco':
+            self.color = color
 
 class Motor:
 
